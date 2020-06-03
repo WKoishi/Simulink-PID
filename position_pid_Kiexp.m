1618ed946de0f45e0ea13a1b11cb3eabf14dc328
WKoishi <intelwzt@outlook.com>
2020-06-03 22:55:00 +0800

    文件名改成"position_pid"

--- position_pid_Kiexp.m
+++ position_pid_Kiexp.m
@@ -2,7 +2,7 @@ clear
 i=0;
 for Ki=0.01:0.09:0.28
     i=i+1;
-    Out=sim('simple_pid');  % slx文件名
+    Out=sim('position_pid');  % slx文件名
     subplot(2,2,i);
     plot(Out.simout)
     title(['Ki = ',num2str(Ki)]);
